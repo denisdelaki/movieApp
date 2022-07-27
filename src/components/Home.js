d1586f5240cb33a13060398cded48b99e2d6e40d
denisdelaki <denisdelaki@gmail.com>
2022-07-27 09:02:22 +0300

    added the movie child to the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import MovieList from "./MovieList";
 import About from "./About";
-function Home() {
+import Search from "./Search";
+function Home({handleSearch}) {
+    const [movies, setMovies] = useState([])
+    
+    useEffect(() => {
+        fetch("http://www.omdbapi.com/?s=blood&apikey=30d6f0cb")
+            .then(res => res.json())
+        .then(data=>setMovies(data.Search))
+    }, [])
   return (
     <div>
       <NavBar />
-      <MovieList />
+      <Search onChange={handleSearch} />
+      <MovieList movies={movies} />
       <About />
     </div>
   );
