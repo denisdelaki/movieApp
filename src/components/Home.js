157d697b35218c6a22113a86c831bb671d103513
denisdelaki <denisdelaki@gmail.com>
2022-07-28 02:07:52 +0300

    added the child component to hold the movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import Movies from "./Movies";
 function Home() {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?s=blood&apikey=30d6f0cb`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search));
+  }, [])
   return (
     <div>
-      <h>Home</h>
+      <Movies movies={movies}/>
    </div>
   );
 }
