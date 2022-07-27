620e764b88d2a6790ff94a17f06739768a37b5a3
denisdelaki <denisdelaki@gmail.com>
2022-07-27 23:00:37 +0300

    created the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from "react";
-import NavBar from "./NavBar";
-import MovieList from "./MovieList";
-import About from "./About";
-import Search from "./Search";
+import React from "react";
+
 function Home() {
-    const [movies, setMovies] = useState([])
-    const [searchMovie, setSearch] = useState("")
-    
-    useEffect(() => {
-        fetch("http://www.omdbapi.com/?s=$red&apikey=30d6f0cb")
-          .then((res) => res.json())
-          .then((data) => setMovies(data.Search));
-    }, [searchMovie])
   return (
     <div>
-      <NavBar />
-          <Search searchMovie={searchMovie} setSearch={setSearch}/>
-      <MovieList movies={movies} />
-      <About />
-    </div>
+      <h>Home</h>
+   </div>
   );
 }
+
 export default Home;
