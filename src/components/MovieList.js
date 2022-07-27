016e9b62d3b7e6999c5442b3b00c6d74362fd47d
denisdelaki <denisdelaki@gmail.com>
2022-07-27 09:01:29 +0300

    modified the movie list and the layout

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,73 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../App.css";
-function MovieList() {
-  const [movies, setMovies] = useState([]);
-  const [isWatched, setWatched] = useState("Unwatched");
-  const [isAdd, setAdd] = useState("Add Favorites");
-  useEffect(() => {
-    fetch("http://www.omdbapi.com/?s=blood&apikey=30d6f0cb")
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
-  }, []);
-  function toggleWatch() {
-    setWatched((isWatched) => !isWatched);
-  }
-  function addFavorites() {
-    setAdd((isAdd) => !isAdd);
-  }
-  return (
-    <div
-      id="cardContainer"
-      style={{
-        display: "block",
-      }}
-    >
-      <div id="movieContainer">
-        {movies.map((movie) => (
-          <div key={movie.id} className="movieCard">
-            <img className="moviePoster" src={movie.Poster} alt={movie.Title} />
-            <h3>Movie Description</h3>
-            <h5 className="movieTitle">Title: {movie.Title}</h5>
-            <h5>Type: {movie.Type}</h5>
-            <h5>Year: {movie.Year}</h5>
-            <span>
-              <button
-                style={{
-                  marginLeft: "0rem",
-                  color: "red",
-                  background: "black",
-                  border: "none",
-                  fontSize: "1.1rem",
-                  width: "8rem",
-                  height: "3rem",
-                  cursor: "progress",
-                  borderRadius: "1.5rem",
-                }}
-                onClick={addFavorites}
-              >
-                {isAdd ? "Add Favorites" : "Added"}
-              </button>
-              <button
-                onClick={toggleWatch}
-                style={{
-                  marginLeft: "0.5rem",
-                  color: "red",
-                  background: "black",
-                  border: "none",
-                  fontSize: "1.2rem",
-                  width: "8rem",
-                  height: "3rem",
-                  cursor: "progress",
-                  borderRadius: "1.5rem",
-                }}
-              >
-                {isWatched ? "UnWatched" : "Watched"}
-              </button>
-            </span>
-          </div>
-        ))}
+
+function MovieList(props) {
+    
+    return (
+      <div id="cardContainer">
+        <div id="movieContainer">
+          {props.movies.map((movie) => (
+            <div className="movieCard">
+              <img src={movie.Poster} alt={movie.Title} />
+              <h3>Movie Description</h3>
+              <h5>Title: {movie.Title}</h5>
+              <h5>Type: {movie.Type}</h5>
+              <h5>Year: {movie.Year}</h5>
+              <span>
+                <button
+                  style={{
+                    marginLeft: "0rem",
+                    color: "red",
+                    background: "black",
+                    border: "none",
+                    fontSize: "1.1rem",
+                    width: "8rem",
+                    height: "3rem",
+                    cursor: "progress",
+                    borderRadius: "1.5rem",
+                  }}
+                >
+                  Add Favorites
+                </button>
+                <button
+                  style={{
+                    marginLeft: "0.5rem",
+                    color: "red",
+                    background: "black",
+                    border: "none",
+                    fontSize: "1.2rem",
+                    width: "8rem",
+                    height: "3rem",
+                    cursor: "progress",
+                    borderRadius: "1.5rem",
+                  }}
+                >
+                  Watch
+                </button>
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );  
 }
-export default MovieList;
+export default MovieList
